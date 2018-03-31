0fd6bd38122dc7d6ebe3db4ac642fb679a78beae
xarvh <francesco.orsenigo@gmail.com>
2018-03-31 15:48:12 +0200

    Clean up Mech svg

--- src/MechSvg.elm
+++ src/MechSvg.elm
@@ -36,10 +36,18 @@ mech : Float -> String -> String -> Svg a
 mech angle brightColor darkColor =
     let
         bodyStyle =
-            ""
+            styles
+            [ "fill:" ++ darkColor
+            , "stroke:" ++ brightColor
+            , "stroke-width:0.03"
+            ]
 
         eyesStyle =
-            ""
+            styles
+            [ "fill:#f80000"
+            , "stroke:#ff8383"
+            , "stroke-width:0.00553906"
+            ]
     in
     g
         [ transform "scale(3,-3) translate(0,-287.00002)" ]
@@ -48,12 +56,12 @@ mech angle brightColor darkColor =
             , x "-0.34999204"
             , height "0.26498201"
             , width "0.083487481"
-            , style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , bodyStyle
             ]
             []
         , rect
             [ transform "scale(-1,1)"
-            , style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , bodyStyle
             , width "0.083487481"
             , height "0.26498201"
             , x "-0.35230175"
@@ -65,7 +73,7 @@ mech angle brightColor darkColor =
             , x "-0.42440477"
             , height "0.23229113"
             , width "0.20145902"
-            , style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , bodyStyle
             ]
             []
         , rect
@@ -73,12 +81,12 @@ mech angle brightColor darkColor =
             , x "-0.29396078"
             , height "0.16940348"
             , width "0.6519469"
-            , style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , bodyStyle
             ]
             []
         , rect
             [ transform "scale(-1,1)"
-            , style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , bodyStyle
             , width "0.20145902"
             , height "0.23229113"
             , x "-0.42671448"
@@ -86,7 +94,7 @@ mech angle brightColor darkColor =
             ]
             []
         , rect
-            [ style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            [ bodyStyle
             , width "0.20277083"
             , height "0.23613821"
             , x "-0.38636273"
@@ -98,7 +106,7 @@ mech angle brightColor darkColor =
             , x "0.18590166"
             , height "0.23613821"
             , width "0.20277083"
-            , style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , bodyStyle
             ]
             []
         , ellipse
@@ -106,12 +114,12 @@ mech angle brightColor darkColor =
             , cy "286.99118"
             , rx "0.084701769"
             , ry "0.16940352"
-            , style "fill:#7c7c7c;fill-opacity:1;stroke:#c1c1c1;stroke-width:0.03;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , bodyStyle
             ]
             []
         , ellipse
             [ transform "rotate(13.994631)"
-            , style "fill:#f80000;fill-opacity:1;stroke:#ff8383;stroke-width:0.00553906;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , eyesStyle
             , ry "0.027490584"
             , rx "0.017793473"
             , cy "278.37259"
@@ -123,7 +131,7 @@ mech angle brightColor darkColor =
             , cy "284.72101"
             , rx "0.017793473"
             , ry "0.027490584"
-            , style "fill:#f80000;fill-opacity:1;stroke:#ff8383;stroke-width:0.00553906;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , eyesStyle
             , transform "rotate(7.151609)"
             ]
             []
@@ -132,13 +140,13 @@ mech angle brightColor darkColor =
             , cy "278.37772"
             , rx "0.017793473"
             , ry "0.027490584"
-            , style "fill:#f80000;fill-opacity:1;stroke:#ff8383;stroke-width:0.00553906;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , eyesStyle
             , transform "matrix(-0.97031839,0.24183097,0.24183097,0.97031839,0,0)"
             ]
             []
         , ellipse
             [ transform "matrix(-0.9922202,0.12449527,0.12449527,0.9922202,0,0)"
-            , style "fill:#f80000;fill-opacity:1;stroke:#ff8383;stroke-width:0.00553906;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+            , eyesStyle
             , ry "0.027490584"
             , rx "0.017793473"
             , cy "284.72379"
