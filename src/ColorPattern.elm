599fdb0f221315431023539cb401c0673c0c0901
xarvh <francesco.orsenigo@gmail.com>
2018-04-19 19:32:24 +0200

    Add sol, octarine

--- src/ColorPattern.elm
+++ src/ColorPattern.elm
@@ -11,11 +11,13 @@ type alias ColorPattern =
 patterns : List ColorPattern
 patterns =
     [ { bright = "#f00", dark = "#900", key = "red" }
+    , { bright = "orange", dark = "blue", key = "sol" }
     , { bright = "#0f0", dark = "#090", key = "green" }
     , { bright = "#00f", dark = "#009", key = "blue" }
     , { bright = "#0ee", dark = "#0bb", key = "cyan" }
     , { bright = "#f0f", dark = "#909", key = "purple" }
     , { bright = "#ee0", dark = "#bb0", key = "yellow" }
+    , { bright = "#0ee", dark = "purple", key = "octarine" }
     ]
 
 
