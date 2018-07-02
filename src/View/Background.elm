6a3d676ffe719acf1d869eef5497625eeff13781
xarvh <francesco.orsenigo@gmail.com>
2018-07-02 22:57:48 +0200

    hex

--- src/View/Background.elm
+++ src/View/Background.elm
@@ -4,9 +4,12 @@ import Game exposing (..)
 import Math.Vector2 as Vec2 exposing (Vec2, vec2)
 import Random exposing (Generator)
 import Random.Extra
-import Svg exposing (Svg)
-import Svg.Attributes
+import Svg as S
+import Svg.Attributes as SA
 import View exposing (..)
+
+
+-- trash
 
 
 type alias Rect =
@@ -19,116 +22,66 @@ type alias Rect =
     }
 
 
-
--- Random generation
-
-
-intToRgb : Int -> String
-intToRgb n =
-    "rgb(" ++ String.fromInt n ++ "," ++ String.fromInt n ++ "," ++ String.fromInt n ++ ")"
-
-
-sizeGenerator : Generator Float
-sizeGenerator =
-    Random.float 0.5 3
-
-
-classGenerator : Generator String
-classGenerator =
-    let
-        floatToClass n =
-            if n < 0.03 then
-                "background-path"
-            else
-                ""
-    in
-    Random.float 0 1 |> Random.map floatToClass
-
-
-rectGenerator : Int -> Int -> Generator Rect
-rectGenerator x y =
-    let
-        s =
-            1
-
-        makeRect xx yy ( w, h ) color class =
-            { x = xx
-            , y = yy
-            , w = w
-            , h = h
-            , color = color
-            , class = class
-            }
-    in
-    Random.map5 makeRect
-        (Random.float -s s |> Random.map (\d -> d + toFloat x))
-        (Random.float -s s |> Random.map (\d -> d + toFloat y))
-        (Random.map2 Tuple.pair sizeGenerator sizeGenerator)
-        (Random.int 210 250 |> Random.map intToRgb)
-        classGenerator
-
-
 initRects : Random.Seed -> { a | halfWidth : Int, halfHeight : Int } -> List Rect
 initRects seed { halfWidth, halfHeight } =
-    let
-        generators =
-            List.range -halfWidth halfWidth
-                |> List.map
-                    (\x ->
-                        List.range -halfHeight halfHeight
-                            |> List.map
-                                (\y ->
-                                    rectGenerator x y
-                                )
-                    )
-
-        generator =
-            generators
-                |> List.concat
-                |> Random.Extra.combine
-    in
-    Random.step generator seed
-        |> Tuple.first
-
-
-
--- draw
-
-
-drawRect : Rect -> Svg a
-drawRect rect =
-    Svg.rect
-        [ x rect.x
-        , y rect.y
-        , width rect.w
-        , height rect.h
-        , fill "none"
-        , strokeWidth 0.04
-        , stroke rect.color
-        , Svg.Attributes.class rect.class
-        ]
-        []
+    [ { x = -halfWidth |> toFloat
+      , y = -halfHeight |> toFloat
+      , w = halfWidth * 2 |> toFloat
+      , h = halfHeight * 2 |> toFloat
+      , color = ""
+      , class = ""
+      }
+    ]
 
 
 
 --
 
 
-terrain : List Rect -> Svg a
+viewRect rect =
+    S.rect
+        [ fill "url(#hex-background)"
+        , x rect.x
+        , y rect.y
+        , width rect.w
+        , height rect.h
+        ]
+        []
+
+
+terrain : List Rect -> S.Svg a
 terrain rects =
-    rects
-        |> List.map drawRect
-        |> Svg.g []
-
-
-xterrain : (Vec2 -> Float -> Bool) -> List Rect -> Svg a
-xterrain isWithinViewport rects =
     let
-        shouldRender : Rect -> Bool
-        shouldRender { x, y, w, h } =
-            isWithinViewport (vec2 (x + w / 2) (y + h / 2)) (max w h)
+        ( fill, stroke, strokeWidth ) =
+            ( "#fff", "#eee", "3px" )
     in
-    rects
-        --|> List.filter shouldRender
-        |> List.map drawRect
-        |> Svg.g []
+    S.g
+        []
+        [ S.defs
+            []
+            [ S.pattern
+                [ SA.id "hex-background"
+                , SA.width "56px"
+                , SA.height "100px"
+                , SA.patternUnits "userSpaceOnUse"
+                , SA.patternTransform "scale(0.02)"
+                ]
+                [ S.rect
+                    [ SA.width "100%"
+                    , SA.height "100%"
+                    , SA.fill fill
+                    ]
+                    []
+                , S.path
+                    [ SA.d "M28 66L0 50L0 16L28 0L56 16L56 50L28 66L28 100"
+                    , SA.fill fill
+                    , SA.stroke stroke
+                    , SA.strokeWidth strokeWidth
+                    ]
+                    []
+                ]
+            ]
+        , rects
+            |> List.map viewRect
+            |> S.g []
+        ]
